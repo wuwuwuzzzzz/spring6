09919005a2f26480e81f70480432cae8d8029e9f
wxz <759126349@qq.com>
2024-01-10 16:11:05 +0800

    Initial commit

---
+++ spring-first/src/main/java/com/wxz/User.java
@@ -0,0 +1,2 @@
+package com.wxz;public class User {
+}
---
+++ spring-first/src/main/java/com/wxz/UserTest.java
@@ -0,0 +1,18 @@
+package com.wxz;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 测试 User 类
+ *
+ * @author wxz
+ * @date 16:07 2024/1/10
+ */
+public class TestUser
+{
+    @Test
+    public void testUserObject()
+    {
+
+    }
+}
