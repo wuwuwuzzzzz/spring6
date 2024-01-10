e27b7b5f5e10a97a680b3cb5d35e13b0dba8f3d1
wxz <759126349@qq.com>
2024-01-10 16:11:31 +0800

    入门案例

--- spring-first/src/main/java/com/wxz/User.java
+++ spring-first/src/main/java/com/wxz/User.java
@@ -1,2 +1,18 @@
-package com.wxz;public class User {
+package com.wxz;
+
+/**
+ * @author wxz
+ * @date 16:03 2024/1/10
+ */
+public class User
+{
+
+    /**
+     * @author wxz
+     * @date 16:04 2024/1/10
+     */
+    public void add()
+    {
+        System.out.println("add.....");
+    }
 }
--- spring-first/src/main/java/com/wxz/UserTest.java
+++ spring-first/src/main/java/com/wxz/UserTest.java
@@ -1,6 +1,7 @@
 package com.wxz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 测试 User 类
@@ -8,11 +9,19 @@ import org.junit.jupiter.api.Test;
  * @author wxz
  * @date 16:07 2024/1/10
  */
-public class TestUser
+public class UserTest
 {
     @Test
     public void testUserObject()
     {
+        // 加载 Spring 配置文件，对象创建
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        // 获取创建的对象
+        User user = (User) context.getBean("user");
 
+        System.out.println(user);
+
+        // 测试对象方法
+        user.add();
     }
 }
