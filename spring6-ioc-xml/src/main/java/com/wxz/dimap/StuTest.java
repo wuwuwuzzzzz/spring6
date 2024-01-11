ea4a38460d734535781534109073696ddf5ccc51
wxz <759126349@qq.com>
2024-01-11 20:11:08 +0800

    基于 XML 管理 Bean - 依赖注入 - p 命名空间

--- spring6-ioc-xml/src/main/java/com/wxz/dimap/StuTest.java
+++ spring6-ioc-xml/src/main/java/com/wxz/dimap/StuTest.java
@@ -32,4 +32,16 @@ public class StuTest
         Student student = context.getBean("student", Student.class);
         System.out.println(student);
     }
+
+    /**
+     * @author wxz
+     * @date 19:58 2024/1/11
+     */
+    @Test
+    public void testP()
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-diref.xml");
+        Student student = context.getBean("student2", Student.class);
+        System.out.println(student);
+    }
 }
